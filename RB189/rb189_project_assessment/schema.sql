6ee7fcb49c14ae6a0d2f75e7735934997c26a678
Chase Sobey <christopher.chase.sobey@gmail.com>
2023-03-26 00:06:21 +0000

    schema updated with tables

--- RB189/rb189_project_assessment/schema.sql
+++ RB189/rb189_project_assessment/schema.sql
@@ -1,2 +1,14 @@
-CREATE DATABASE apartments (
-id serial PRIMARY KEY);
+CREATE TABLE apartment_buildings (
+id serial PRIMARY KEY,
+name text UNIQUE NOT NULL,
+total_units INTEGER NOT NULL
+address text UNIQUE NOT NULL
+);
+
+
+CREATE TABLE units (
+id serial PRIMARY KEY,
+building_id integer NOT NULL REFERENCES apartment_buildings.id,
+rent NUMERIC(6,2) NOT NULL,
+tenant text UNIQUE NOT NULL
+);
