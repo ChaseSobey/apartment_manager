bb6e16695107aeef28f35263d4e9bf72d7e0da91
Chase Sobey <christopher.chase.sobey@gmail.com>
2023-03-26 03:18:23 +0000

    Addition of data to database

--- RB189/rb189_project_assessment/schema.sql
+++ RB189/rb189_project_assessment/schema.sql
@@ -12,3 +12,13 @@ building_id integer NOT NULL REFERENCES apartment_buildings.id,
 rent NUMERIC(6,2) NOT NULL,
 tenant text UNIQUE NOT NULL
 );
+
+INSERT INTO apartment_buildings (name, total_units, address) VALUES
+('Fillmore Center', 15, '1420 Turk St'),
+('Vintage Pads', 10, '14 E Border Place'),
+('Baker Street apts', 4, '12 Baker St');
+
+INSERT INTO units (building_id, rent, tenant) VALUES
+(1, 2650, 'Stephanie Smith'), (1, 2650, 'James Earl'), (1, 1700, 'Tiffany Mueller'), (1, 3600, 'Zach Brown'),
+(2, 950, 'Bowen Young'), (2, 650, 'Callie Norton'), (2, 800, 'Marissa Kumani'),
+(3, 1200, 'Marianna Lopez'), (3, 1200, 'Neda Heydari'), (3, 1200, 'Alexa Flint'), (3, 900, 'Martin Jones');
